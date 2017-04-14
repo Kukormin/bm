bf72f43d98b3b7f99f2ad417fcc63dd01a0d8245
Кукормин Тимофей <tim.kukom@gmail.com>
2017-04-14 15:35:38 +0400

    Импорт

--- local/lib/Catalog/Products.php
+++ local/lib/Catalog/Products.php
@@ -27,8 +27,12 @@ class Products
 	 */
 	const CACHE_PATH = 'Local/Catalog/Products/';
 
+	/**
+	 * @var null текущие файлы, добавленные импортом
+	 */
 	private static $issetFiles = null;
-	private static $currentProductId = null;
+
+	private static $summary = [];
 
 	/**
 	 * Возвращает все товары
@@ -67,20 +71,17 @@ class Products
 				'NAME',
 				'CODE',
 				'XML_ID',
-				'PREVIEW_PICTURE',
 				'PROPERTY_PRICE',
 				'PROPERTY_PRICE_WO_DISCOUNT',
 				'PROPERTY_MODEL',
 				'PROPERTY_COLOR',
 				'PROPERTY_DISABLED',
+				'PROPERTY_PICTURE',
 				'PROPERTY_PICTURES',
 				'PROPERTY_FILES',
 			];
 			$propSelect = Props::getForSelect();
-
-
 			$select = array_merge($select, $propSelect);
-
 
 			$props = Props::getAll();
 
@@ -90,7 +91,7 @@ class Products
 			$rsItems = $iblockElement->GetList($sort, $filter, false, $nav, $select);
 			while ($item = $rsItems->Fetch())
 			{
-
+				// TODO: сделать эффективную выборку получения разделов
 				$sections = [];
 				$sectionsDB = \CIBlockElement::GetElementGroups($item['ID']);
 				while ($section = $sectionsDB->Fetch())
@@ -102,20 +103,19 @@ class Products
 					'NAME' => $item['NAME'],
 					'CODE' => $item['CODE'],
 					'SECTIONS' => $sections,
-					'PREVIEW_PICTURE' => $item['PREVIEW_PICTURE'],
 					'XML_ID' => $item['XML_ID'],
 					'PRICE' => intval($item['PROPERTY_PRICE_VALUE']),
 					'PRICE_WO_DISCOUNT' => intval($item['PROPERTY_PRICE_WO_DISCOUNT_VALUE']),
 					'MODEL' => intval($item['PROPERTY_MODEL_VALUE']),
 					'COLOR' => intval($item['PROPERTY_COLOR_VALUE']),
 					'DISABLED' => $item['PROPERTY_DISABLED_VALUE'] == 1,
+					'PICTURE' => $item['PROPERTY_PICTURE_VALUE'],
 					'PICTURES' => $item['PROPERTY_PICTURES_VALUE'],
 					'FILES' => $item['PROPERTY_FILES_VALUE'],
 				];
 
 				foreach ($props['ITEMS'] as $prop)
 				{
-
 					$product['PROPS'][$prop['ID']] = $item['PROPERTY_PROP_' . $prop['ID'] . '_VALUE'];
 				}
 				$return['BY_XML_ID'][$item['XML_ID']] = $item['ID'];
@@ -159,196 +159,132 @@ class Products
 	 */
 	public static function import($items)
 	{
+		self::initFiles();
 
-		$issetFiles = [];
-		$f = \CFile::GetList([], []);
-
-		while ($file = $f->Fetch())
-		{
-			$issetFiles[$file['ORIGINAL_NAME']] = $file['ID'];
-		}
-
-		self::$issetFiles = $issetFiles;
+		self::$summary = [
+			'ADD' => 0,
+			'UPDATE' => 0,
+			'SAME' => 0,
+			'FILES' => 0,
+		];
 
 		foreach ($items as $item)
 		{
+			$fields = self::getIbFields($item);
 			$element = self::getByXmlId($item['XML_ID']);
 			if (!$element)
 			{
-				self::itemAdd($item);
+				self::itemAdd($fields);
 			}
 			else
 			{
-				self::$currentProductId = $element['ID'];
-				self::itemUpdate($item);
+				self::itemUpdate($element, $fields);
 			}
 		}
+	}
+
+	/**
+	 * Преобразует данные импорта в поля ИБ
+	 * @param $item
+	 * @return array
+	 */
+	public static function getIbFields($item)
+	{
+		$files = self::getItemFiles($item['FILES']);
+		$price = ($item['PRICE_ACTION']) ? $item['PRICE_ACTION'] : $item['PRICE'];
+		$prWoAction = ($item['PRICE'] > $price) ? $item['PRICE'] : $price;
+		$model = Models::getByXmlId($item['MODEL']);
+		$color = Colors::getByXmlId($item['COLOR']);
+
+		$propValues = [
+			'PRICE' => $price,
+			'PRICE_WO_DISCOUNT' => $prWoAction,
+			'MODEL' => $model['ID'],
+			'COLOR' => $color['ID'],
+			'DISABLED' => $item['DISABLED'],
+			'PICTURE' => $files['preview'],
+			'PICTURES' => $files['images'],
+			'FILES' => $files['files'],
+		];
+		foreach ($item['PROPS'] as $key => $value)
+		{
+			$prop = Props::getByXmlId($key);
+			$propValues['PROP_' . $prop['ID']] = $value;
+		}
+		$fields = [
+			'IBLOCK_ID' => self::IBLOCK_ID,
+			'NAME' => $item['NAME'],
+			'XML_ID' => $item['XML_ID'],
+			'IBLOCK_SECTION' => self::getItemSections($item['SECTIONS']),
+			'PROPERTY_VALUES' => $propValues,
+		];
+
+		return $fields;
 	}
 
 	/**
 	 * Добавляет элемент продукт
-	 * @param $item
+	 * @param $fields
 	 */
-	public static function itemAdd($item)
+	public static function itemAdd($fields)
 	{
-
-		$files = self::getItemFiles($item['FILES']);
-		$price = ($item['PRICE_ACTION']) ? $item['PRICE_ACTION'] : $item['PRICE'];
-		$prWoAction = ($item['PRICE_ACTION'] > $price) ? $item['PRICE_ACTION'] : $price;
-
-		$item['IBLOCK_ID'] = self::IBLOCK_ID;
-		$item['IBLOCK_SECTION'] = self::getItemSections($item['SECTIONS']);
-		$item['PREVIEW_PICTURE'] = $files['preview'];
-		$item['PROPERTY_VALUES'] = [
-			'PRICE' => $price,
-			'PRICE_WO_DISCOUNT' => $prWoAction,
-			'MODEL' => Models::getByXmlId($item['MODEL']),
-			'COLOR' => Colors::getByXmlId($item['COLOR']),
-			'DISABLED' => $item['DISABLED'],
-			'PICTURES' => $files['images'],
-			'FILES' => $files['files'],
-		];
-		$prodId = new \CIBlockElement();
-
-		foreach ($item['PROPS'] as $key => $value)
-		{
-			$prop = Props::getByXmlId($key);
-			$item['PROPERTY_VALUES']['PROP_' . $prop['ID']] = $value;
-		}
-		$prodId->Add($item);
+		$be = new \CIBlockElement();
+		$be->Add($fields);
+		self::$summary['ADD']++;
 	}
 
 	/**
 	 * Обновляет продукт
-	 * @param $item
+	 * @param $old
+	 * @param $new
 	 */
-	public static function itemUpdate($item)
+	public static function itemUpdate($old, $new)
 	{
-		$toUpdate = [];
+		$updateFields = [];
 		$updateProperties = [];
-		$product = self::getById(self::$currentProductId);
+		$propValues = $new['PROPERTY_VALUES'];
 
-		$files = self::getItemFiles($item['FILES']);
-		$element = new \CIBlockElement();
+		// Название
+		if ($old['NAME'] != $new['NAME'])
+			$updateFields['NAME'] = $new['NAME'];
+		// Разделы
+		if (!self::checkArrays($old['SECTIONS'], $new['IBLOCK_SECTION']))
+			$updateFields['IBLOCK_SECTION'] = $new['IBLOCK_SECTION'];
+		// Цены
+		if ($old['PRICE'] != $propValues['PRICE'])
+			$updateProperties['PRICE'] = $propValues['PRICE'];
+		if ($old['PRICE_WO_DISCOUNT'] != $propValues['PRICE_WO_DISCOUNT'])
+			$updateProperties['PRICE_WO_DISCOUNT'] = $propValues['PRICE_WO_DISCOUNT'];
+		// Модель
+		if ($old['MODEL'] != $propValues['MODEL'])
+			$updateProperties['MODEL'] = $propValues['MODEL'];
+		// Цвет
+		if ($old['COLOR'] != $propValues['COLOR'])
+			$updateProperties['COLOR'] = $propValues['COLOR'];
+		// Не действует
+		if ($old['DISABLED'] != $propValues['DISABLED'])
+			$updateProperties['DISABLED'] = $propValues['DISABLED'];
+		// Картинка для списка
+		if ($old['PICTURE'] != $propValues['PICTURE'])
+			$updateProperties['PICTURE'] = ['VALUE' => $propValues['PICTURE']];
+		// Картинки
+		if (!self::checkArrays($old['PICTURES'], $propValues['PICTURES']))
+			$updateProperties['PICTURES'] = $propValues['PICTURES'];
+		// Файлы
+		if (!self::checkArrays($old['FILES'], $propValues['FILES']))
+			$updateProperties['FILES'] = $propValues['FILES'];
 
-		if ($product['NAME'] != $item['NAME'])
+		if ($updateFields || $updateProperties)
 		{
-			$toUpdate['NAME'] = $item['NAME'];
-		}
-
-		$sections = self::getItemSections($item['SECTIONS']);
-		$newSections = self::checkArrays($product['SECTIONS'], $sections);
-		if ($newSections)
-		{
-			$toUpdate['IBLOCK_SECTION'] = $newSections;
-		}
-
-		/*if ($product['PREVIEW_PICTURE'] != $files['preview']['ID'])
-		{
-			$toUpdate['PREVIEW_PICTURE'] = $files['preview']['ID'];
-		}*/
-
-		$itemPrice = (empty($item['PRICE'])) ? 0 : floatval($item['PRICE']);
-		$itemActionPrice = (empty($item['PRICE_ACTION'])) ? 0 : $item['PRICE_ACTION'];
-
-		if ($itemActionPrice < $itemPrice)
-		{
-			$price = $itemActionPrice;
-			$prWoAction = $itemPrice;
+			$be = new \CIBlockElement();
+			if ($updateFields)
+				$be->Update($old['ID'], $updateFields);
+			if ($updateProperties)
+				$be->SetPropertyValuesEx($old['ID'], self::IBLOCK_ID, $updateProperties);
+			self::$summary['UPDATE']++;
 		}
 		else
-		{
-			$price = $itemPrice;
-			$prWoAction = $price;
-		}
-
-		if ($product['PRICE'] != $price)
-		{
-			$updateProperties['PRICE'] = $price;
-		}
-
-		if ($product['PRICE_WO_DISCOUNT'] != $prWoAction)
-		{
-			$updateProperties['PRICE_WO_DISCOUNT'] = $prWoAction;
-		}
-
-		$newModel = Models::getByXmlId($item['MODEL']);
-		if ($product['MODEL'] != $newModel['ID'])
-		{
-			$updateProperties['MODEL'] = $newModel['ID'];
-		}
-
-		$newColor = Colors::getByXmlId($item['COLOR']);
-		if ($product['COLOR'] != $newColor['ID'])
-		{
-			$updateProperties['COLOR'] = $newColor['ID'];
-		}
-
-		$newDisabled = ($item['DISABLED'] == 'Нет') ? false : true;
-		if ($product['DISABLED'] != $newDisabled)
-		{
-			$updateProperties['DISABLED'] = $item['DISABLED'];
-		}
-
-		$newPictures = self::checkArrays($product['PICTURES'], $files['images']);
-
-		if ($newPictures)
-		{
-			if (count($newPictures) < count($product['PICTURES']))
-			{
-				$del = [];
-				$to_dell = array_diff($product['PICTURES'], $newPictures);
-				foreach ($to_dell as $img)
-				{
-					$del[$img] = [
-						'VALUE' => [
-							'MODULE_ID' => 'iblock',
-							'del' => 'Y',
-						],
-					];
-				}
-				$newPictures = $del;
-			}
-			else
-			{
-				$imgs = [];
-				foreach ($newPictures as $img)
-				{
-					$imgs[$img] = ['VALUE' => \CFile::MakeFileArray(\CFile::GetPath($img))];
-				}
-				$newPictures = $imgs;
-			}
-			$updateProperties['PICTURES'] = $newPictures;
-		}
-
-
-		$newFiles = self::checkArrays($product['FILES'], $files['FILES']);
-		if ($newFiles)
-		{
-			$updateProperties['FILES'] = $newFiles;
-		}
-
-		foreach ($item['PROPS'] as $key => $value)
-		{
-			$prop = Props::getByXmlId($key);
-			if ($product[$prop['ID']] != $value)
-			{
-				$updateProperties['PROP_' . $prop['ID']];
-			}
-
-		}
-
-		if ($toUpdate)
-		{
-			$element->Update($product['ID'], $toUpdate);
-		}
-
-		if ($updateProperties)
-		{
-			//$toUpdate['PROPERTY_VALUES'] = $updateProperties;
-			$element->SetPropertyValuesEx($product['ID'], self::IBLOCK_ID, $updateProperties);
-			//var_dump($updateProperties);
-		}
+			self::$summary['SAME']++;
 	}
 
 
@@ -363,10 +299,24 @@ class Products
 		foreach ($data as $section)
 		{
 			$cat = Categories::getByXmlId($section);
-			$sections[] = $cat['ID'];
+			if ($cat)
+				$sections[] = $cat['ID'];
 		}
 
 		return $sections;
+	}
+
+	/**
+	 * Получает все файлы, которые были добавлены в систему импортом
+	 */
+	private static function initFiles()
+	{
+		self::$issetFiles = [];
+		$rsFiles = \CFile::GetList([], [
+			'MODULE_ID' => 'products',
+		]);
+		while ($file = $rsFiles->Fetch())
+			self::$issetFiles[$file['ORIGINAL_NAME']] = $file['ID'];
 	}
 
 	/**
@@ -379,44 +329,27 @@ class Products
 		$files = [];
 		foreach ($data as $file)
 		{
-			$filePath = '../upload/files/' . $file['NAME'] . '.' . $file['EXT'];
-			if (file_exists($filePath))
+			$fileName = $file['NAME'] . '.' . $file['EXT'];
+			$filePath = $_SERVER['DOCUMENT_ROOT'] . '/upload/files/' . $fileName;
+			if (!file_exists($filePath))
+				continue;
+
+			if (self::$issetFiles[$fileName])
+				$fileId = self::$issetFiles[$file['NAME'] . '.' . $file['EXT']];
+			else
 			{
-				$fileInfo = [
-					'name' => $file['NAME'] . '.' . $file['EXT'],
-					'size' => filesize($filePath),
-					'tmp_name' => $filePath,
-					'type' => mime_content_type($filePath),
-					'del' => 'Y',
-					'MODULE_ID' => 'products',
-					'content' => file_get_contents($filePath),
-				];
-
-				if (!self::$issetFiles[$file['NAME'] . '.' . $file['EXT']])
-				{
-					$fileId = \CFile::SaveFile($fileInfo, 'products');
-				}
-				else
-				{
-					$fileId = self::$issetFiles[$file['NAME'] . '.' . $file['EXT']];
-				}
-
-				if ($file['TYPE'] == '1')
-				{
-					$files['preview'] = \CFile::MakeFileArray(\CFile::GetPath($fileId));
-					$files['preview']['ID'] = $fileId;
-				}
-
-				if ($file['TYPE'] == '2')
-				{
-					$files['images'][] = $fileId;
-				}
-
-				if ($file['TYPE'] == '3')
-				{
-					$files['files'][] = $fileId;
-				}
+				$fileInfo = \CFile::MakeFileArray($filePath);
+				$fileInfo['MODULE_ID'] = 'products';
+				$fileId = \CFile::SaveFile($fileInfo, 'products');
+				self::$summary['FILES']++;
 			}
+
+			if ($file['TYPE'] == '1')
+				$files['preview'] = $fileId;
+			elseif ($file['TYPE'] == '2')
+				$files['images'][] = $fileId;
+			elseif ($file['TYPE'] == '3')
+				$files['files'][] = $fileId;
 
 		}
 
@@ -426,30 +359,21 @@ class Products
 
 
 	/**
-	 * Сравнение массивов для добавления или удаления элементов
-	 * @param $isset
-	 * @param $new
+	 * Сравнение массивов
+	 * @param $a1
+	 * @param $a2
 	 * @return array
 	 */
-	public static function checkArrays($isset, $new)
+	public static function checkArrays($a1, $a2)
 	{
-		$res = [];
-		$add = array_diff($new, $isset);
-		$del = array_diff($isset, $new);
+		$cnt1 = count($a1);
+		$cnt2 = count($a2);
+		if ($cnt1 != $cnt2)
+			return false;
 
-		if ($add)
-		{
-			$res = array_merge($isset, $add);
-
-			return $res;
-		}
-
-		if ($del)
-		{
-			$res = array_diff($isset, $del);
-
-			return $res;
-		}
+		$intersect = array_intersect($a1, $a2);
+		$cnt3 = count($intersect);
+		return $cnt1 == $cnt3;
 	}
 
 
